88f20ed0315f02429181430d3d08ddc4f5541524
haribabu <haribabu.adra.ext@siemens.com>
2025-08-20 16:03:43 -0400

    testdss

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/pricebook/:recordId', async (req, res) => {
     );
     console.log('@@@',result);
     if (result.rows.length > 0) {
-      res.json(result.rows[0]);
+      res.json(result);
     } else {
       res.status(404).send('Record not found');
     }
